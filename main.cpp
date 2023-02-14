53a4cded7f44654f9d8bf191579723ecd6eb08c5
edwin pardo <124716322+edwin9850@users.noreply.github.com>
2023-02-14 19:48:13 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*   8/02/2023
+autor: edwin sebastian pardo salamanca
+tipo de variables primitivas
+*/
+
+#include <iostream>
+using namespace std;
+
+//funcion principal
+int main() {
+cout<<"*******************************************************"<<endl;
+cout<<"************* tipos fundamentales en c++ **************"<<endl;
+cout<<"*******************************************************"<<endl;
+cout<<"************ tipo de datos numeros de bytes ***********"<<endl;
+cout<<"*******************************************************"<<endl<<endl;
+
+//indica cuanros bytes tiene cada variable  
+cout<<"\t char: "<<sizeof(char)<<endl;  
+cout<<"\t short: "<<sizeof(short)<<endl;  
+cout<<"\t int: "<<sizeof(int)<<endl;  
+cout<<"\t long: "<<sizeof(long)<<endl;  
+cout<<"\t float: "<<sizeof(float)<<endl;  
+cout<<"\t double: "<<sizeof(double)<<endl;
+cout<<"\t long double: "<<sizeof(long double)<<endl;  
+   
+  return 0;
+  }
